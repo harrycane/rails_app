e66c5ee6170ff173643f21d5e7bcb75830947123
Oleg Andreev <mrtwista@yandex.ru>
2016-12-13 19:33:58 +0300

    kostyl to make server run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# Character.create(name: 'Luke', movie: movies.first)
-Images.create(name: 'Ship1', movie: movies.first)
+Image.delete_all
+Image.reset_pk_sequence
+Image.create(
+    [
+        {name: 'Liner1', file: 'assets/images/pictures/liner_1.jpg'},
+        {name: 'Liner2', file: 'assets/images/pictures/liner_2.jpg'},
+        {name: 'Liner3', file: 'assets/images/pictures/liner_3.jpg'},
+        {name: 'Liner4', file: 'assets/images/pictures/liner_4.jpg'},
+        {name: 'Liner5', file: 'assets/images/pictures/liner_5.jpg'},
+        {name: 'Pirate_Ship_1', file: 'assets/images/pictures/pirate_ship_1.jpg'},
+        {name: 'Pirate_Ship_2', file: 'assets/images/pictures/pirate_ship_2.jpg'},
+        {name: 'Pirate_Ship_3', file: 'assets/images/pictures/pirate_ship_3.jpg'},
+        {name: 'Pirate_Ship_4', file: 'assets/images/pictures/pirate_ship_4.jpg'},
+        {name: 'Pirate_Ship_5', file: 'assets/images/pictures/pirate_ship_5.jpg'},
+        {name: 'Yacht_1', file: 'assets/images/pictures/yacht_1.jpg'},
+        {name: 'Yacht_2', file: 'assets/images/pictures/yacht_2.jpg'},
+        {name: 'Yacht_3', file: 'assets/images/pictures/yacht_3.jpg'},
+        {name: 'Yacht_4', file: 'assets/images/pictures/yacht_4.jpg'},
+        {name: 'Yacht_5', file: 'assets/images/pictures/yacht_5.jpg'}
+    ]
+)
+
+Theme.delete_all
+Theme.reset_pk_sequence
+Theme.create(
+    [
+        {name: 'Liners'},
+        {name: 'Pirate Ships'},
+        {name: 'Yachts'}
+    ]
+)
 
